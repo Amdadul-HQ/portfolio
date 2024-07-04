b20934f599d52c32e69d8cbfb81bbc28fd471076
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-04 23:42:01 +0600

    adding logo in navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import Hamburger from 'hamburger-react'
+import Image from "next/image";
 
 const Navbar = () => {
     const pathName = usePathname()
@@ -30,12 +31,13 @@ const Navbar = () => {
     ]
     const [isOpen, setOpen] = useState(false)
   return (
-    <nav className="container mx-auto py-2 flex justify-between items-center border-b border-purple-300 px-5 rounded-xl shadow-xl">
+    <nav className="container mx-auto py-4 flex justify-between items-center border-b border-purple-300 px-5 rounded-xl shadow-xl">
       <div className="text-gray-200 flex flex-col max-w-44 cursor-pointer">
-        <h1 className="lg:text-3xl text-xl text-justify font-medium tracking-[7px]">Rimon</h1>
-        <p className="text-gray-400 tracking-wide uppercase text-[9px] lg:text-xs font-normal">create you unique</p>
+        <Image src='/riwhitelogo.png' width={140} height={80} alt=""/>
+        {/* <h1 className="lg:text-3xl text-xl text-justify font-medium tracking-[7px]">Rimon</h1>
+        <p className="text-gray-400 tracking-wide uppercase text-[9px] lg:text-xs font-normal">create you unique</p> */}
       </div>
-      <ul className={`text-gray-400 lg:text-sm text-xl lg:gap-y-0 gap-y-5 lg:text-left text-right flex lg:flex-row flex-col  transition-all duration-1000 ${isOpen ? 'right-6 top-20' : '-right-full top-20'} gap-x-14 lg:static fixed font-normal`}>
+      <ul className={`text-gray-400 lg:text-sm text-xl mt-2 lg:gap-y-0 gap-y-5 lg:text-left text-right flex lg:flex-row flex-col  transition-all duration-1000 ${isOpen ? 'right-6 top-20' : '-right-full top-20'} gap-x-14 lg:static fixed font-normal`}>
         {
             links.map((link,index )=> <Link className={`${pathName === link.path && "text-white"} hover:text-white transition-all duration-300`} href={link.path} key={index}>{link.title}</Link>  )
         }
