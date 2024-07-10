80bad789e32adc85d85b0c9a024bd4ce7c4bbaf0
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-10 23:43:29 +0600

    adding navbar style

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
       // console.log(scrolled);
       if(scrolled<610){
         setpathname('#home')
-        // if(scrolled>)
+        if(scrolled>115){
+          setActive(true)
+        }
       }
       if(scrolled>=610 && scrolled<1200){
         setpathname('#skills')
@@ -69,13 +71,13 @@ const Navbar = () => {
     ]
     const [isOpen, setOpen] = useState(false)
   return (
-    <nav className="container fixed max-w-screen-xl mx-auto py-4 flex justify-between items-center border-b border-purple-300 px-5 rounded-xl shadow-xl">
+    <nav className={`container z-50 ${active && 'bg-[rgb(14,16,29)]'} fixed max-w-screen-xl mx-auto py-4 flex justify-between items-center border-b border-purple-300 px-5 rounded-xl shadow-xl`}>
       <div className="text-gray-200 flex flex-col max-w-44 cursor-pointer">
         <Image src='/riwhitelogo.png' width={140} height={80} alt=""/>
         {/* <h1 className="lg:text-3xl text-xl text-justify font-medium tracking-[7px]">Rimon</h1>
         <p className="text-gray-400 tracking-wide uppercase text-[9px] lg:text-xs font-normal">create you unique</p> */}
       </div>
-      <ul className={`text-gray-400 lg:text-sm text-xl mt-2 lg:gap-y-0 gap-y-5 lg:text-left text-right flex lg:flex-row flex-col  transition-all duration-1000 ${isOpen ? 'right-6 top-20' : '-right-full top-20'} gap-x-14 lg:static fixed font-normal`}>
+      <ul className={`text-gray-400 z-50 lg:text-sm text-xl mt-2 lg:gap-y-0 gap-y-5 lg:text-left text-right flex lg:flex-row flex-col  transition-all duration-1000 ${isOpen ? 'right-6 top-20' : '-right-full top-20'} gap-x-14 lg:static fixed font-normal`}>
         {
             links.map((link,index )=> <Link className={`${pathname === link.path && "text-white"} hover:text-white transition-all duration-300`} href={link.path} key={index}>{link.title}</Link>  )
         }
